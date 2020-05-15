157b44d7e9994605825a4a847a38bcb620461631
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-05-15 12:14:19 -0400

    first commit

--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -3,7 +3,7 @@ package virtual_pet;
 public class VirtualPetApplication {
 
     public static void main(String[] args) {
-        //Interact with a VirtualPet object in this method
+        System.out.println("Congratulations on your new addition to your family!");
     }
 
 }
