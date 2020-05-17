efb3b468743192a169cc334921568a588e1df7e4
Sara Thornsberry <sarathornsberry@saras-mbp.home>
2020-05-16 20:34:00 -0400

    added another method and while user increases level, others decrease.

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -3,55 +3,73 @@ package virtual_pet;
 import java.util.Random;
 
 public class VirtualPet {
+    void petStatus() {
+        System.out.println("Hunger: " + hunger);
+        System.out.println("Thirst: " + thirst);
+        System.out.println("Boredom: " + boredom);
+        System.out.println("Tiredness: " + tiredness);
+    }
+
     public String name;
     public int hunger;
     public int thirst;
     public int boredom;
+    public int tiredness;
 
     Random rand = new Random();
 
     public VirtualPet(String defaultName) {
         name = defaultName;
-        hunger = rand.nextInt(100);
-        thirst = rand.nextInt(100);
-        boredom = rand.nextInt(100);
+        hunger = rand.nextInt(80);
+        thirst = rand.nextInt(80);
+        boredom = rand.nextInt(80);
+        tiredness = rand.nextInt(80);
     }
 
     //Getter
     public String getName() {
         return name;
     }
+
     public int getHunger() {
         return hunger;
     }
+
     public int getThirst() {
         return thirst;
     }
+
     public int getBoredom() {
         return boredom;
+    }
+
+    public int getTiredness() {
+        return tiredness;
     }
 
     //Setter
     public void setName(String newName) {
         this.name = newName;
     }
-    public int setHunger(int newHunger) {
+
+    public void setHunger(int newHunger) {
         this.hunger = newHunger;
-        return newHunger;
     }
+
     public void setThirst(int newThirst) {
         this.thirst = newThirst;
     }
+
     public void setBoredom(int newBoredom) {
         this.boredom = newBoredom;
     }
 
-    //Tick to loop the game
-    public void tick() {
-        hunger += 15;
-        thirst += 15;
-        boredom += 30;
+    public void setTiredness(int newTiredness) {
+        this.tiredness = newTiredness;
     }
+
+    //Case Statements
+
 
 
 }
--- src/main/java/virtual_pet/VirtualPetApplication.java
+++ src/main/java/virtual_pet/VirtualPetApplication.java
@@ -12,70 +12,76 @@ public class VirtualPetApplication {
 
         System.out.println("Awww, " + petName + " is sooo cute!");
         System.out.println("It's super important for you to care for " + petName +
-                            ". To keep them happy, you must try to keep their food, thirst, and boredom level " +
-                            "above 60, but less than 100.");
-
+                "." + '\n' + "To keep them happy, you must try to keep all of their levels " +
+                "above 50, but less than 100.");
 
 
         int select;
+        virtualPet.petStatus();
+
 
         do {
             System.out.println("What would you like to do with " + petName + "?");
             System.out.println("Press [1] to feed " + petName + ".");
             System.out.println("Press [2] to quench " + petName + "'s thirst.");
             System.out.println("Press [3] to play with " + petName + ".");
+            System.out.println("Press [4] to get some sleep.");
             System.out.println("Press [0] to break " + petName + "'s heart and exit the game.");
 
-            //In the do/while loop, add the pet's current status to the [1]-[4].
-
             select = input.nextInt();
+
 
             if (select == 1) {
                 System.out.println("Thank you for the delicious meal!");
                 int hunger = virtualPet.getHunger();
-                int addHunger = 15;
+                int thirst = virtualPet.getThirst();
                 virtualPet.setHunger(hunger + 15);
-                System.out.println("Hunger: " + hunger);
-                    if (virtualPet.getHunger() > 100) {
-                        System.out.println("Whoa, my belly hurts. I can't eat another bite.");
-                        continue;
-
-                    }
-            }
-            else if (select == 2) {
+                virtualPet.setThirst(thirst - 5);
+                if (virtualPet.getHunger() > 100) {
+                    System.out.println("Whoa, my belly hurts. I can't eat another bite.");
+                    continue;
+                }
+            } else if (select == 2) {
                 System.out.println("Thank you for my drink!");
                 int thirst = virtualPet.getThirst();
-                int addThirst = 10;
+                int hunger = virtualPet.getHunger();
                 virtualPet.setThirst(thirst + 10);
-                System.out.println("Thirst: " + thirst);
-                    if (virtualPet.getThirst() > 100) {
-                        System.out.println("You're gonna make me have an accident!");
-                        continue;
-                    }
-            }
-            else if (select == 3) {
+                virtualPet.setHunger(hunger - 3);
+                if (virtualPet.getThirst() > 100) {
+                    System.out.println("You're gonna make me have an accident!");
+                    continue;
+                }
+            } else if (select == 3) {
                 System.out.println("Yay! I love to play!");
                 int boredom = virtualPet.getBoredom();
-                int addBoredom = 25;
+                int hunger = virtualPet.getHunger();
+                int thirst = virtualPet.getThirst();
                 virtualPet.setBoredom(boredom + 25);
-                System.out.println("Boredom: " + boredom);
+                virtualPet.setHunger(hunger - 5);
+                virtualPet.setThirst(thirst - 7);
                 if (virtualPet.getBoredom() > 100) {
                     System.out.println("Too...tired...to play. I need a good Emu nap!");
                     continue;
                 }
-            }
-            else if (select == 0) {
+            } else if (select == 4) {
+                System.out.println("I'm whooped...goodnight!");
+                int tiredness = virtualPet.getTiredness();
+                int boredom = virtualPet.getBoredom();
+                virtualPet.setTiredness(tiredness + 30);
+                virtualPet.setBoredom(boredom - 8);
+                if (virtualPet.getTiredness() > 100) {
+                    System.out.println("I'm ready to get up!");
+                    continue;
+                }
+            } else if (select == 0) {
                 System.out.println("You're leaving already? See you soon!");
                 return;
             }
 
-            } while (select != 0);
+            virtualPet.petStatus();
 
+        } while (select != 0);
 
-        virtualPet.tick();
 
     }
-
-
-
 }
